51ec11dd0dae4010e9b6a5c8696e26366c30753b
Natko Biscan <nbiscan7@gmail.com>
2024-10-14 14:03:49 +0200

    Initial Commit

---
+++ RSS Feed App/RSS_Feed_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSS_Feed_AppApp.swift
+//  RSS Feed App
+//
+//  Created by Natko Biscan on 14.10.2024..
+//
+
+import SwiftUI
+
+@main
+struct RSS_Feed_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
